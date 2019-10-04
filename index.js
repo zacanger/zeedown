1892548687a005ffafea63d1c6e93fa17563396f
zacanger <zac@zacanger.com>
2019-10-04 13:32:48 -0600

    chore: cleanup

--- index.js
+++ index.js
@@ -1,16 +1,10 @@
 /* eslint-disable no-useless-escape */
 
-const zeedown = (str = '', shrt = false) => {
-  const emReg = /_(.*?)_/g
-  const emRep = '<em>$1</em>'
-  const strongReg = /\*(.*?)\*/g
-  const strongRep = '<strong>$1</strong>'
-  const delReg = /~(.*?)~/g
-
-  return str
-    .replace(strongReg, strongRep) // strong
-    .replace(emReg, emRep) // em
-    .replace(delReg, '<del>$1</del>') // strike
+const zeedown = (str = '') =>
+  str
+    .replace(/\*(.*?)\*/g, '<strong>$1</strong>') // strong
+    .replace(/_(.*?)_/g, '<em>$1</em>') // em
+    .replace(/~(.*?)~/g, '<del>$1</del>') // strike
     .replace(/\n`{3}([\S]+)?\n([\s\S]+)\n`{3}/g, (a, b, text) => `\n<pre><code>${text.trim()}</code></pre>`) // code block
     .replace(/`(.*?)`/g, '<code>$1</code>') // inline code
     .replace(/\n\*(.*)/g, (_, text) => `\n<ul>\n<li>${text.trim()}</li>\n</ul>`) // ul
@@ -20,6 +14,5 @@ const zeedown = (str = '', shrt = false) => {
     .replace(/<\/ol>\s?<ol>/g, '') // clean up extra ol
     .replace(/<\/blockquote>\s?<blockquote>/g, '\n') // clean up extra blockquote
     .replace(/\n\s*\n/g, '\n') // collapse consecutive newlines
-}
 
 module.exports = zeedown
